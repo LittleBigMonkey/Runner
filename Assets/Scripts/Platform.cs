6ff8c033d92a1a1579ef896a82cd8b21338897f0
Little Big Monkey Studio <info@littlebigmonkey.com>
2019-11-09 19:53:27 +0100

    Basic platform creation

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour, IActivable
+{
+    public float lifetime = 5.0f;
+
+    public void Activate()
+    {
+        Destroy(gameObject, lifetime); //do stuff like particles
+    }
+
+    public void Reset()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,15 +3,27 @@
 public class PlayerInput : MonoBehaviour
 {
     public float tolerance = 0.1f;
+    public Platform platform;
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            var collider = Physics2D.OverlapCircle(Camera.main.ScreenToWorldPoint(Input.mousePosition), tolerance);
+            var point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var collider = Physics2D.OverlapCircle(point, tolerance);
 
-            if (collider?.GetComponent<Destroyable>())
-                collider?.GetComponent<Destroyable>().Activate();
+            if (collider)
+                collider?.GetComponent<Destroyable>()?.Activate();
+            else
+                CreatePlatform(point);
         }
+    }
+
+    void CreatePlatform(Vector2 position)
+    {
+        position.x = Mathf.Floor(position.x) + 0.5f; //align to grid
+        position.y = Mathf.Floor(position.y) + 0.5f;
+
+        Instantiate(platform, position, Quaternion.identity).Activate();
     }
 }
